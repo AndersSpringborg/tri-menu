653b26d45817e10f71a7da93955d6d17d88f4a8f
Anders Aaen Springborg <aasp@trifork.com>
2024-01-03 20:26:12 +0100

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from 'drizzle-kit';
+
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "mysql2",
+  dbCredentials: {
+    uri: process.env.DATABASE_URL!,
+  },
+  tablesFilter: ["tri-menu_*"],
+} satisfies Config;
